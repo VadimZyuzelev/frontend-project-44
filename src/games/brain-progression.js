6f662b7d82574718e4d37f12d894ed429be2682e
VadimZyuzelev <zyuzelev97@gmial.com>
2023-08-01 16:07:42 +0300

    Убрал комментарии из функции brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,22 +3,17 @@ const brainProgression = (numFirst, numCount, step) => {
   let randomIndexElement = 0;
   let numLast = 0;
 
-  // Вычисляем последний элемент прогрессии
   numLast = numFirst + step * (numCount - 1);
 
   for (let i = numFirst; i <= numLast; i += step) {
     arrayProgression[0].push(i);
   }
 
-  // Получаем рандомный индекс элемента
   randomIndexElement = Math.floor(Math.random() * arrayProgression[0].length);
 
   for (let i = 0; i < arrayProgression[0].length; i += 1) {
     if (i === randomIndexElement) {
-      // Записываем элемент в результирующий массив
       arrayProgression[1] = arrayProgression[0][i];
-
-      // Изменяем элемент
       arrayProgression[0][i] = '..';
     }
   }
